485bb20862e0d6f23c7a337039f4e687e73bed5c
Trenton Wuerker <trenton.wuerker@gmail.com>
2017-07-24 15:43:37 -0600

    added cards and modal

--- app.js
+++ app.js
@@ -1,11 +1,36 @@
 
 
 $(document).ready(() => {
+  $('.modal').modal()
   $.get('https://quiet-badlands-94685.herokuapp.com/games')
   .then((data) => {
     console.log(data);
     for (var i = 0; i < data.length; i++) {
-      $('ul').append(`<li>${data[i].name}, published by ${data[i].publisher} in ${data[i].release}</li>`)
+      $('#cards').append(`
+          <div class="col s12 m6">
+            <div class="card blue-grey darken-1">
+              <div class="card-content white-text">
+                <span class="card-title">${data[i].name}</span>
+                <p>Published by: ${data[i].publisher}</p>
+                <p>Release Year: ${data[i].release}</p>
+              </div>
+              <div class="card-action">
+                <a href="#">Edit</a>
+                <a href="#">Delete</a>
+              </div>
+            </div>
+          </div>
+      `)
     }
   })
+})
+
+$('#welcomeButton').click(function () {
+  document.getElementById('welcome').style.display = 'none'
+  document.getElementById('cards').style.display = 'block'
+})
+
+$('#addCard').click(function () {
+  console.log('hello');
+  $('#modal1').modal('open')
 })
