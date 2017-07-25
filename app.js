249b5a1b2f1f83a982b317502d80009e48f6064b
Trenton Wuerker <trenton.wuerker@gmail.com>
2017-07-25 10:34:11 -0600

    added single card return

--- app.js
+++ app.js
@@ -2,15 +2,17 @@
 
 $(document).ready(() => {
   $('.modal').modal()
+})
+
+$('#welcomeButton').click(function () {
   $.get('https://quiet-badlands-94685.herokuapp.com/games')
   .then((data) => {
-    console.log(data);
     for (var i = 0; i < data.length; i++) {
       $('#cards').append(`
           <div class="col s12 m6">
             <div class="card blue-grey darken-1">
               <div class="card-content white-text">
-                <span class="card-title">${data[i].name}</span>
+                <span class="card-title">${i + 1}. ${data[i].name}</span>
                 <p>Published by: ${data[i].publisher}</p>
                 <p>Release Year: ${data[i].release}</p>
               </div>
@@ -23,11 +25,32 @@ $(document).ready(() => {
       `)
     }
   })
-})
-
-$('#welcomeButton').click(function () {
   document.getElementById('welcome').style.display = 'none'
   document.getElementById('cards').style.display = 'block'
+})
+
+$('#specificButton').click(() => {
+  let id = $('#searchBox').val()
+  $.get(`https://quiet-badlands-94685.herokuapp.com/games/${id}`).then((data) => {
+    console.log(data);
+    $('#cards').append(`
+        <div class="col s12 m6">
+          <div class="card blue-grey darken-1">
+            <div class="card-content white-text">
+              <span class="card-title">${data[0].name}</span>
+              <p>Published by: ${data[0].publisher}</p>
+              <p>Release Year: ${data[0].release}</p>
+            </div>
+            <div class="card-action">
+              <a href="#" id="edit">Edit</a>
+              <a href="#" id="delete" >Delete</a>
+            </div>
+          </div>
+        </div>
+    `)
+  })
+    document.getElementById('welcome').style.display = 'none'
+    document.getElementById('cards').style.display = 'block'
 })
 
 $('#addCard').click(function () {
